a28f4327f59125a5a8cd68722f04fef30f988cb4
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-08-04 18:15:15 -0300

    add material-ui no projeto, adicionando botão para teste

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 
 function App() {
   return (
     <>
+      <h1>Teste</h1>
+      <FormularioCadastro />
     </>
   );
 }
